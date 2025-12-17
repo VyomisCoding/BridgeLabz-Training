b2ee7dd2c33e6ff5893144ce94a5c9a7cb9163bc
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-18 00:25:58 +0530

    [Vyomesh Pandey] Add a code to find the power of any number.

---
+++ core-java-practice/gcr-codebase/programming-elements/powerCalculation.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class powerCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base number: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter the exponent number: ");
+        double exponent = sc.nextDouble();
+        double result = Math.pow(base, exponent);
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+    
+}
