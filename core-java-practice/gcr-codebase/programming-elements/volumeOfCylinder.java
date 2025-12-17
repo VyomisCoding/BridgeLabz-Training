c37592dea99a59741e01c6f4be8bd9a8bd8b7b67
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-18 00:12:29 +0530

    [Vyomesh Pandey] Add the code to find the Volume of Cylinder.

---
+++ core-java-practice/gcr-codebase/programming-elements/volumeOfCylinder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class volumeOfCylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double pie = 3.14;
+        System.out.print("Enter the Radius: ");
+        Double radius = sc.nextDouble();
+        System.out.print("Enter the height: ");
+        Double height = sc.nextDouble();    
+        Double Volume = pie * radius * radius* height;
+        System.out.print("the volume of the cylinder is :" + Volume );
+        
+    }
+}
