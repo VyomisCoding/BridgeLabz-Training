5b9b6258b4780e3550dc4dbb9eaa5901c1179286
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-18 00:07:15 +0530

    [Vyomesh Pandey] Add the code to find the Area of Circle

---
+++ core-java-practice/gcr-codebase/programming-elements/areaOfCircle.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class areaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double pievalue = 3.14;
+        System.out.println("Enter the radius of the circle:");
+        Double radiusofcircle=sc.nextDouble();
+        Double areaofcircle= pievalue*radiusofcircle*radiusofcircle;
+        System.out.println("Area of Circle is: " + areaofcircle);
+    }
+}
