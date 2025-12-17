cbbbcb56b7561a566c4a8595a6c8b454ac184787
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-17 23:15:48 +0530

    [Vyomesh Pandey] deleted the unwanted file

---
+++ core-java-practice/gcr-codebase/programming-elements/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    // Main method: program execution starts from here
+    public static void main(String[] args) {
+
+        // Printing a message on the console
+        System.out.println("Welcome to Bridgelabz");
+    }
+}
