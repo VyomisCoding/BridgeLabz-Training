a61608a1e39935d949127aebf5b0adb732796e33
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-18 00:30:14 +0530

    [Vyomesh Pandey] Add a Welcome code.

--- core-java-practice/gcr-codebase/programming-elements/main.java
+++ core-java-practice/gcr-codebase/programming-elements/main.java
@@ -2,8 +2,6 @@ public class main {
 
     // Main method: program execution starts from here
     public static void main(String[] args) {
-
-        // Printing a message on the console
         System.out.println("Welcome to Bridgelabz");
     }
 }
