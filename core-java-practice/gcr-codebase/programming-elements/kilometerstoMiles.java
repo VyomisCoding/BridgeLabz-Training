8850829de0b990afa0dae3665b0be64dba48d2eb
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-18 12:51:47 +0530

    [Vyomesh Pandey] Add a code to convert kilometers to miles

---
+++ core-java-practice/gcr-codebase/programming-elements/kilometerstoMiles.java
@@ -0,0 +1,8 @@
+public class kilometerstoMiles {
+    public static void main(String[] args) {
+        double kilometers = 10.0; // Example value in kilometers
+        double miles = kilometers * 0.621371; // Conversion factor
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+    }
+    
+}
