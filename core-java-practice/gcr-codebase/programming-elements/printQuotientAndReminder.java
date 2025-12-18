55f9ab5d021c3758971384e269e77f0c78609673
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-18 16:10:44 +0530

    [Vyomesh Pandey] Add a program to calculate the quotient and remainder of two integers based on user input

---
+++ core-java-practice/gcr-codebase/programming-elements/printQuotientAndReminder.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class printQuotientAndReminder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the dividend: ");
+        int dividend = sc.nextInt();
+        System.out.print("Enter the divisor: ");
+        int divisor = sc.nextInt();
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("The quotient is " + quotient + " and the remainder is " + remainder);
+    }
+}
