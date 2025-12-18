4a6f458ad3d05b84400d9fef53c033cd37e06044
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-18 12:51:20 +0530

    [Vyomesh Pandey] Add a code to calculate average percentage marks

---
+++ core-java-practice/gcr-codebase/programming-elements/averagePercentMarks.java
@@ -0,0 +1,10 @@
+public class averagePercentMarks {
+    public static void main(String[] args) {
+        int mathsMarks = 94;
+        int physicsMarks = 95;
+        int chemMarks = 96;
+        int totalMarks = mathsMarks + physicsMarks + chemMarks;
+        double averagePercent = (totalMarks / 3.0);
+        System.out.println("Average Percentage Marks: " + averagePercent + "%");
+    }
+}
