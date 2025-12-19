5141159a59646564813641acd378d5ae665db5a8
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-19 23:41:23 +0530

    [Vyomesh Pandey]  Add. Implement a program to display multiples of a number below 100 using a while loop

---
+++ core-java-practice/gcr-codebase/control-flows/MultipleOfNumberBelow100UsingWhile.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class MultipleOfNumberBelow100UsingWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number less than 100: ");
+        int number = sc.nextInt();
+
+        if (number <= 0 || number >= 100) {
+            System.out.println("Enter a positive number less than 100.");
+        } else {
+            int counter = 100;
+
+            System.out.println("Multiples of " + number + " below 100 are:");
+
+            while (counter > 1) {
+                if (counter % number == 0) {
+                    System.out.println(counter);
+                }
+                counter--;
+            }
+        }
+    }
+}
