8a1312b1bbd211ff780a27acc98529dcc6f6d39f
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-21 01:06:01 +0530

    [Vyomesh Pandey] Add. A code to implement a multiplication table generator.

--- core-java-practice/gcr-codebase/arrays/MultiplicationTable.java
+++ core-java-practice/gcr-codebase/arrays/MultiplicationTable.java
@@ -1,4 +1,19 @@
 
-public class MultiplicationTable {
+import java.util.*;
 
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number for multiplication table: ");
+        int number = sc.nextInt();
+        int [] table = new int[10];
+        for (int i = 0; i < 10; i++) {
+            table[i] = number * (i + 1);
+        }
+        System.out.println("Multiplication Table of " + number + ":");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(number + " x " + (i + 1) + " = " + table[i]);
+        }
+    }
+    
 }
