bbae57bc8560262ccae4b8ba4fcb475d78490621
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-21 16:20:10 +0530

    [Vyomesh pandey] Updated. Changed few lines in code which was wrong earlier

--- core-java-practice/gcr-codebase/arrays/VotingEligibilityForStudents.java
+++ core-java-practice/gcr-codebase/arrays/VotingEligibilityForStudents.java
@@ -4,17 +4,10 @@ public class VotingEligibilityForStudents {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int[] studentsAge = new int[10];
-        System.out.println("Enter the age of 10 students:");
-        for(int i=0; i<10; i++) {
         for (int i = 0; i < studentsAge.length; i++) {
             System.out.print("Enter age of student " + (i + 1) + ": ");
             studentsAge[i] = sc.nextInt();
         }
-        System.out.println("Students eligible to vote (age 18 or older):");
-        for(int age : studentsAge) {
-            if(age >= 18) {
-                System.out.println(age);
-
         System.out.println("Voting Eligibility Result");
         for (int age : studentsAge) {// Checking voting eligibility
             if (age < 0) {
