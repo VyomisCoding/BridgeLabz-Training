5ec138c8d5374213e46fa23cfa90924cad8ad9e6
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-24 16:28:53 +0530

    [ Vyomesh Pandey ] Add.  A code for finding quotient and remainder.

---
+++ core-java-practice/gcr-codebase/methods/QuotientAndReminder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class QuotientAndReminder {
+	
+	public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int quotient = number / divisor;
+        int remainder = number % divisor;
+        return new int[]{remainder, quotient};
+    }
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        System.out.print("Enter the divisor: ");
+        int div = sc.nextInt();
+        
+        int[] result = findRemainderAndQuotient(num, div);
+        System.out.println("Quotient = " + result[1]);
+        System.out.println("Remainder = " + result[0]);
+	}
+
+}
