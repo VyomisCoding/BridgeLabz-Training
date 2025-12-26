2579d314bcd0ec78582d76f6037bb51be0b3e512
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-26 15:49:58 +0530

    [ Vyomesh Pandey ] Add. A code for bus route distance tracker.

---
+++ core-java-practice/scenario-based/LuckyDraw.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class LuckyDraw {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int visitorCount = 1;
+        while (true) {
+
+            System.out.print("\nVisitor " + visitorCount + " - Enter your lucky number (or -1 to stop): ");
+            int luckyNumber = sc.nextInt();
+            if (luckyNumber == -1){ // condition to stop the loop
+                System.out.println("Lucky draw closed. Thanks for visiting the mela!");
+                break;
+            }
+
+            if (luckyNumber <= 0){ // invalid input check
+                System.out.println("Invalid number! Please enter a positive number.");
+                continue;
+            }
+            
+            if (luckyNumber % 3 == 0 && luckyNumber % 5 == 0){ // check for divisibility by 3 and 5
+                System.out.println("Congratulations! You won a Diwali gift");
+            } else {
+                System.out.println("Better luck next time");
+            }
+
+            visitorCount++;
+        }
+	}
+
+}
