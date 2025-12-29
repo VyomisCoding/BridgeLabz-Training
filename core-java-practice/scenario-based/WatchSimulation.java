3746c0840fef0c3d2a761d3aaddb9752c24979d4
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-29 11:51:15 +0530

    [ Vyomesh Pandey ] Add. A code for simulate a 24 hrs watch.

--- core-java-practice/scenario-based/WatchSimulation.java
+++ core-java-practice/scenario-based/WatchSimulation.java
@@ -1,9 +1,17 @@
-
+import java.util.*;
 public class WatchSimulation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int hour = 0; hour < 24; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
 
+                System.out.printf("%02d:%02d\n", hour, minute);
+
+                if (hour == 13 && minute == 0) {
+                    System.out.println("⚡ Power Cut! Watch stopped.");
+                    return; // stop completely
+                }
+            }
+        }
 	}
-
 }
