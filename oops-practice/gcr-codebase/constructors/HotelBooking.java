02cc52d32497d2ddee02f009a8b3397106a315ae
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-31 15:44:17 +0530

    [ Vyomesh Pandey ] Add. A code for hotel booking system

---
+++ oops-practice/gcr-codebase/constructors/HotelBooking.java
@@ -0,0 +1,27 @@
+public class HotelBooking{
+    String guestName;
+    String roomType;
+    int nights;
+
+    HotelBooking() {
+        guestName = "Guest";
+        roomType = "Standard";
+        nights = 1;
+    }
+
+    HotelBooking(String g, String r, int n) {
+        guestName = g;
+        roomType = r;
+        nights = n;
+    }
+
+    HotelBooking(HotelBooking hb) {
+        guestName = hb.guestName;
+        roomType = hb.roomType;
+        nights = hb.nights;
+    }
+
+    void display() {
+        System.out.println(guestName + " booked " + roomType + " for " + nights + " nights");
+    }
+}
