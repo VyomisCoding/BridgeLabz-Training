1b2284b57b0f39e368f36f03e7a78c7684d639cc
Vyomesh pandey <vyompandey.955@gmail.com>
2025-12-31 15:42:12 +0530

    [ Vyomesh Pandey ] Add. A code for car rental system

---
+++ oops-practice/gcr-codebase/constructors/CarRental.java
@@ -0,0 +1,16 @@
+public class CarRental{
+    String customerName;
+    String carModel;
+    int rentalDays;
+    double costPerDay = 1000;
+
+    CarRental(String c, String m, int d) {
+        customerName = c;
+        carModel = m;
+        rentalDays = d;
+    }
+
+    void totalCost() {
+        System.out.println("Total Cost: ₹" + (rentalDays * costPerDay));
+    }
+}
