04fb8f7dec450ded97b368cc9f90bab6cf24b877
Vyomesh pandey <vyompandey.955@gmail.com>
2026-01-22 22:54:26 +0530

    [Vyomesh Pandey] Add. A code of Removing Duplicates in List Interface in collections.

---
+++ collection-practice/gcr-codebase/collections/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class RemoveDuplicates{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter elements:");
+        for(int i=0;i<n;i++){
+            list.add(sc.nextInt());
+        }
+        Set<Integer> set = new LinkedHashSet<>(list);      // LinkedHashSet preserves order + removes duplicates
+        List<Integer> result = new ArrayList<>(set);
+        System.out.println("List after removing duplicates: " + result);
+    }
+}
