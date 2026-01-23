dbdbbe08e5e735e5dc493b76c5090be97e848e53
Vyomesh pandey <vyompandey.955@gmail.com>
2026-01-23 11:20:06 +0530

    [ Vyomesh Pandey] Add. A code of Subset Check in Set Interface.

---
+++ collection-practice/gcr-codebase/collections/SubsetCheck.java
@@ -0,0 +1,22 @@
+// Set Interface
+
+import java.util.*;
+
+public class SubsetCheck{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        System.out.print("Enter size of Set1: ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter Set1:");
+        for (int i = 0; i < n1; i++) set1.add(sc.nextInt());
+
+        System.out.print("Enter size of Set2: ");
+        int n2 = sc.nextInt();
+        System.out.println("Enter Set2:");
+        for (int i = 0; i < n2; i++) set2.add(sc.nextInt());
+
+        System.out.println("Is Set1 a subset of Set2? " + set2.containsAll(set1));
+    }
+}
