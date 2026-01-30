16ebba9281b7a33baba72b1c785de7c588374b40
Vyomesh pandey <vyompandey.955@gmail.com>
2026-01-30 15:28:57 +0530

    [Vyomesh Pandey]Add. A code of Extract Links.

---
+++ collection-practice/gcr-codebase/regex-junit/regex-problems/ExtractLinks.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractLinks{
+    public static void main(String[] args){
+        String text = "Visit https://www.google.com and http://example.org for more info.";
+        String regex = "https?://[A-Za-z0-9./_-]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        System.out.println("Links Found:");
+        while(matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
